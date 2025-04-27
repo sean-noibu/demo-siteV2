c15797c46de10eacb558e031ac3aad5001fdd7de
sean-noibu <sean.mallia@noibu.com>
2025-04-27 13:56:49 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ const params = new URLSearchParams(window.location.search);
  * GTM
  */
 const gtm = document.createElement('gtm');
-script.src = (function (w, d, s, l, i) {
+gtm.src = (function (w, d, s, l, i) {
   w[l] = w[l] || [];
   w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
   var f = d.getElementsByTagName(s)[0],
